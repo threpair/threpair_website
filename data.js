57254dcc6d9bd335eb6b09c4565e1b81af84270d
TH.Repair <info@threpair.de>
2025-08-24 12:23:13 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,1 @@
-const PRODUCTS=[
-  {title:"iPhone 11 – Display A‑Qualität",price:159,img:"bilder/iphone11.jpg"},
-  {title:"iPhone 12 – Akku‑Tausch",price:69,img:"bilder/iphone12.jpg"},
-  {title:"Galaxy S20 – Display A",price:199,img:"bilder/galaxy_s20.jpg"},
-  {title:"Backcover‑Tausch (Glas)",price:null,img:"bilder/backcover.jpg"},
-];
 
-const PRICE_DATA={
-  "Apple":{
-    "iPhone 11":{"Display A‑Qualität":159},
-    "iPhone 12":{"Akku‑Tausch":69}
-  },
-  "Samsung":{
-    "Galaxy S20":{"Display A":199}
-  }
-};
